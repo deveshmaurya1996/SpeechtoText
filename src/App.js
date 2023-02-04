017800f77c1cc6432f87c26e65d90c73b46946cb
Devesh Maurya <deveshmaurya1996@gmail.com>
2023-02-04 09:49:33 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+/* eslint-disable no-plusplus */
+import {Component} from 'react'
+import './App.css'
+import {FaMicrophoneAlt} from 'react-icons/fa'
+
+class App extends Component {
+  state = {
+    text: '',
+    recording: false,
+    translatedText: '',
+  }
+
+  componentDidMount() {
+    window.SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition
+
+    this.recognition = new window.SpeechRecognition()
+    this.recognition.interimResults = true
+    this.recognition.lang = 'en-IN'
+
+    this.recognition.addEventListener('result', event => {
+      let text = ''
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+          text += event.results[i][0].transcript
+        }
+      }
+      this.setState({text})
+    })
+  }
+
+  startRecording = () => {
+    this.setState({recording: true})
+    this.recognition.start()
+  }
+
+  stopRecording = () => {
+    this.setState({recording: false})
+    this.recognition.stop()
+  }
+
+  translateToHindi = async () => {
+    const {text} = this.state
+    console.log(text)
+    const subscriptionKey = '99a0eadf725d408ea653c948d14b07d6'
+    const endpoint = 'https://api.cognitive.microsofttranslator.com/'
+    const path = `/translate?api-version=3.0&to=hi&text=${encodeURI(text)}`
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+      },
+    }
+
+    try {
+      const response = await fetch(endpoint + path, requestOptions)
+      const json = await response.json()
+      this.setState({translatedText: json[0].text})
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render() {
+    const {text, recording, translatedText} = this.state
+    const backColor = recording
+      ? 'stop-record-button-color'
+      : 'start-record-button-color'
+    return (
+      <>
+        <div className="app-container">
+          <h1 className="heading">Speech-to-Text and Translation</h1>
+
+          <div className="buttons">
+            <button
+              type="button"
+              className={`record-button ${backColor}`}
+              onClick={recording ? this.stopRecording : this.startRecording}
+            >
+              {recording ? 'Stop' : 'Start'} Recording
+            </button>
+            {recording ? <FaMicrophoneAlt className="mic-icon" /> : ''}
+
+            <button
+              type="button"
+              className="translate-button"
+              onClick={this.translateToHindi}
+            >
+              Translate to Hindi
+            </button>
+          </div>
+          <p className="text-display">{text}</p>
+          <p className="text-display">{translatedText}</p>
+        </div>
+      </>
+    )
+  }
+}
+
+export default App
